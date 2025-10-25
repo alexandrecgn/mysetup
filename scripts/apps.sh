7da189f160dd5749432e54bd3753b881c1898ad1
Alexandre Cavalcanti <alexandrecgn@proton.me>
2025-10-25 11:35:41 -0300

    Adicionar arquivos de configuração

--- scripts/apps.sh
+++ scripts/apps.sh
@@ -5,9 +5,8 @@ sudo apt upgrade -y ;
 sudo flatpak update -y ;
 sudo snap refresh ;
 
-sudo snap install \
-code --classic \
-obsidian --classic ;
+sudo snap install code --classic ;
+sudo snap install obsidian --classic ;
 
 sudo flatpak install -y \
 io.github.flattool.Warehouse \
@@ -16,19 +15,25 @@ app.zen_browser.zen \
 io.gitlab.adhami3310.Impression \
 io.dbeaver.DBeaverCommunity \
 org.gnome.World.PikaBackup \
+org.chromium.Chromium ;
 
 sudo apt install -y \
 zsh \
 starship \
+tealdeer \
+logiops \
 bat \
 btop \
 fzf \
 ripgrep \
 gnome-tweaks \
+gnome-shell-extensions \
 gnome-shell-extension-manager \
 gimp \
 git \
 git-lfs \
 nmap \
 kitty \
-qgis
+qgis ;
+
+tldr --update
