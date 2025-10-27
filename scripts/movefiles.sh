1940d47a58e10c16f48af3e3f0b67e2e93518f55
Alexandre Cavalcanti <alexandrecgn@proton.me>
2025-10-26 23:27:53 -0300

    Atualizar movefiles.sh

--- scripts/movefiles.sh
+++ scripts/movefiles.sh
@@ -1,8 +1,10 @@
 #! /bin/bash
 
-rm ~/.gitconfig;
-rm ~/.zshrc
+rm ~/.gitconfig ;
+rm ~/.zshrc ;
+rm .config/starship.toml ;
 
-mv config_files/zshrc ~/.zshrc;
-mv config_files/gitconfig ~/.gitconfig;
-mv config_files/logid.cfg ~/.config
+mv config_files/zshrc ~/.zshrc ;
+mv config_files/gitconfig ~/.gitconfig ;
+mv config_files/logid.cfg ~/.config ;
+mv config_files/starship.toml ~/.config
